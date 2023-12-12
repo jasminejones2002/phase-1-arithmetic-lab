74402211330ff2eee71a0b96ef5f3af6288321d7
jasminejones2002 <jazziejones02@yahoo.com>
2023-12-12 13:39:11 -0600

    arithmetic lab complete

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+const num1 = 31;
+const num2 = 2;
+var multiply = (num1 * num2);
+console.log(multiply);
 
-// Write your code here
+const random = Math.floor(Math.random() * 10);
+console.log(random);
+
+const num3 = 34;
+const num4 = 10;
+const mod = (num3 % num4);
+console.log(mod);
+
+const max = Math.max(5, 10, 15, 20);
